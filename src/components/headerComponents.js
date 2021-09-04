cd9189587256d901ac3609f4e17024c0b42a20c5
jifryrose <54110752+jifryrose@users.noreply.github.com>
2021-09-04 16:09:53 +0530

    Create headerComponents.js

---
+++ src/components/headerComponents.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+export const Header= styled.div`
+  color: white;
+  align-items:center;
+  background-color: black;
+  display:flex;
+  flex-direction:row;
+  padding: 20px;
+  font-size:20px;
+  justify-content:space-between;
+  font-weight:bold;
+  box-shadow:0 3px 6px 0 #555;
+`;
+export const AppNameComponent= styled.div`
+  display:flex;
+  align-items: center;
+
+`;
+export const SearchComponent= styled.div`
+  display:flex;
+  flex-direction:row;
+  background-color:white;
+  padding:5px;
+  border-radius:3px;
+  ##width:50%;
+`;
+export const SearchInput = styled.input`
+  border:none;
+  outline:none;
+  margin-left: 15px;
+  font-size:15px;
+  font-weight;bold;
+`;
+
